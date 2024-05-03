aec17efe3fa31d84806af3f17ccd3bd36d38b826
Chris Blaszczynski <christopher.blaszczynski@conduent.com>
2024-05-03 15:36:21 -0400

    Changed Constant to be generic and work from anywhere as long as the data files are copied during build.
    (cherry picked from commit 3101c841d8f4c60e4b37201027150385acc50d67)

--- Helpers/Constants.cs
+++ Helpers/Constants.cs
@@ -8,6 +8,6 @@ namespace api_process_runner_api.Helpers
 {
     public static class Constants
     {
-        public const string LocalFilePath = @"C:\temp\Data\";
+        public const string LocalFilePath = @"Data\";
     }
 }
