0d5184caf171de8a2ca2fabf6a4240042ae72050
Chris Blaszczynski <christopher.blaszczynski@conduent.com>
2024-05-03 16:35:11 -0400

    Coded creating the JSON output structure.
    (cherry picked from commit 745d12f94886325778e9d7c5a7594685abde3dae)

--- Util/StepLogger.cs
+++ Util/StepLogger.cs
@@ -1,5 +1,10 @@
 using Azure.Storage.Blobs;
 using FileHelpers;
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System;
+using System.Text.Json;
 
 
 namespace api_process_runner_api.Util;
@@ -26,39 +31,96 @@ public class StepLogger
 { 
     public List<ProcessStep> processSteps = new List<ProcessStep>();
 
-
-    public void AddItem(EppicRecord record, string stepnumber, string steptitle, string status)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="record"></param>
+    /// <param name="stepTitle">Include the Step number in this title</param>
+    /// <param name="status"></param>
+    public void AddItem(EppicRecord record, string stepTitle, string status)
     {
-        ProcessStep step = new ProcessStep
+        var step = (from a in this.processSteps 
+                    where a.Title == stepTitle where a.Status == status select a).FirstOrDefault();
+        step ??= new ProcessStep() { Title = stepTitle, Status = status, EppicRecords = [] };
+
+        step.EppicRecords.Add(record); // This seems impossible: Warning CS8602  Dereference of a possibly null reference.api 
+
+        this.processSteps.Add(step);
+    }
+
+    /* expected output
+{
+  "ProcessStep": {
+    "Title": "Eppic records that failed address lookup",
+    "Status": "recorded",
+    "EppicRecords": [
+      {
+        "PersonID": "5094334",
+        "Phone_Number": "2512271296",
+        "AddressLine1": "705 qfGzfLlf fVE",
+        "AddressLine2": " ",
+        "City": "EVERGREEN",
+        "State": "AL",
+        "ZipCode": "36401"
+      },
+      {
+        "PersonID": "6359555",
+        "Phone_Number": "7866552347",
+        "AddressLine1": "400 pEzdqfL fVE z",
+        "AddressLine2": "APT 209",
+        "City": "CHISHOLM",
+        "State": "MN",
+        "ZipCode": "55719"
+      }
+    ]
+  }
+}     
+     */
+
+    public void PrintData()
+    {
+        var json = JsonSerializer.Serialize(this.processSteps,
+            new JsonSerializerOptions { WriteIndented = true, IncludeFields = true });
+
+        Console.WriteLine(json);
+    }
+
+
+    //public async Task<bool> WriteToBlobAsync(Stream fileStream,string blobName)
+    //{
+    //    // TBD: Try Catch 
+    //    var blobServiceClient = new BlobServiceClient(ConnectionString); 
+    //    var containerClient = blobServiceClient.GetBlobContainerClient(Container);  
+    //    await containerClient.CreateIfNotExistsAsync();
+    //    var blobClient = containerClient.GetBlobClient(blobName);
+    //    await blobClient.UploadAsync(fileStream, true);
+    //    return true;
+
+    //}
+
+    public static class TestStepLogger
+    {
+        public static void Test()
         {
-            Title = $"Step {i} - {steptitle}",
-            Status = status,
-            EppicRecords = new List<EppicRecord> {  new EppicRecord
-                    {
-                        PersonID? = record.PersonID,
-                        Phone_Number = record.Phone_Number}",
-                        AddressLine1 = record.AddressLine1,
-                        AddressLine2 = record.AddressLine2,
-                        City = record.City,
-                        State = record.State,
-                        ZipCode = record.ZipCode
-                    }
-        };
+            var logger = new StepLogger();
+
+            var e1 = new EppicRecord()
+            {
+                PersonID = "1"
+            };
+            var e2 = new EppicRecord()
+            {
+                PersonID = "1"
+            };
+
+            logger.AddItem(e1, "step 1", "passed");
+            logger.AddItem(e1, "step 2", "passed");
+
+            logger.AddItem(e2, "step 1", "passed");
+            logger.AddItem(e2, "step 2", "failed");
+
+            logger.PrintData();
+        }
     }
 
-
-    
-
-
-    public async Task<bool> WriteToBlobAsync(Stream fileStream,string blobName)
-    {
-        // TBD: Try Catch 
-        var blobServiceClient = new BlobServiceClient(ConnectionString); 
-        var containerClient = blobServiceClient.GetBlobContainerClient(Container);  
-        await containerClient.CreateIfNotExistsAsync();
-        var blobClient = containerClient.GetBlobClient(blobName);
-        await blobClient.UploadAsync(fileStream, true);
-        return true;
-
-    }
 }
