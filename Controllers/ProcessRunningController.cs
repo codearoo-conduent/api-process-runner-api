75ce644918c915fc97843820daf1c2601cc2fde0
Chris Blaszczynski <christopher.blaszczynski@conduent.com>
2024-05-03 15:35:49 -0400

    Added API output structure
    (cherry picked from commit b18e0faf69f346f559e359a473f698844a73509f)
    (cherry picked from commit 1c61c3dd4d91a910e16dc9bd66747706fffeec76)

--- Controllers/ProcessRunningController.cs
+++ Controllers/ProcessRunningController.cs
@@ -22,10 +22,19 @@ namespace api_process_runner_api.Controllers
 
 
         [HttpPost()] // Pass the 4 Files that were uploaded
-        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(UploadFileResponse), 200)]
         [ProducesResponseType(400)]
         public async Task<ActionResult> LaunchProcess([FromBody] UploadedFilesRequest filesrequest)
-        { 
+        {
+            /* Sample test request
+{
+    "giactFilename": "GIACT202131107.CSV",
+    "siebelFilename": "Siebel.20231107.CSV",
+    "eppicFilename": "EPPIC.20231107.CSV",
+    "addressFilename": "Hospital-Shelters.20231107.csv"
+}
+             */
+
             try
             {
                 if (filesrequest == null)
@@ -44,10 +53,13 @@ namespace api_process_runner_api.Controllers
                 Console.WriteLine("Ready to Go, let's search for a HospitalByFullAddress. Press Enter!");
                 var recordswithFullAddress = dataHelper.HospitalShelterDataParser.FindHospitalByFullAddress("799 47dH bd", "", "SAN DIEGO", "CA", hospitaldataRecords ?? new List<HospitalShelterRecords>());
                 Console.WriteLine($@"Hospital Found: {recordswithFullAddress?.AddressLine1}");
-                Console.WriteLine("Let's print out all the Hospital Records, press  Enter!");
-                dataHelper.HospitalShelterDataParser.PrintHospitalRecords(hospitaldataRecords ?? new List<HospitalShelterRecords>());
+                Console.WriteLine("Let's print out all the Hospital Records");
+                //dataHelper.HospitalShelterDataParser.PrintHospitalRecords(hospitaldataRecords ?? new List<HospitalShelterRecords>());
                 Console.WriteLine();
-                var response = "all good";
+
+                var response = new UploadFileResponse() 
+                { OutputFilename = $"FraudCheckOutput-{DateTime.UtcNow.ToString("yyyyMMdd")}-{Guid.NewGuid()}.json" };
+
                 return new OkObjectResult(response);
             }
             catch (Exception ex)
